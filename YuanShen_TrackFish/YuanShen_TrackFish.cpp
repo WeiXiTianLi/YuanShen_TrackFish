e8dc83cd189514e26386663915ad9bb32cac907d
GengGode <GengGode@outlook.com>
2021-10-04 22:17:14 +0800

    新建文件夹

--- YuanShen_TrackFish/YuanShen_TrackFish.cpp
+++ YuanShen_TrackFish/YuanShen_TrackFish.cpp
@@ -1,7 +1,49 @@
 #include "YuanShen_TrackFish.h"
 
+#include <QString>
+#include <QMouseEvent>
+#include <QPropertyAnimation>
+
 YuanShen_TrackFish::YuanShen_TrackFish(QWidget *parent)
     : QWidget(parent)
 {
     ui.setupUi(this);
+	this->setWindowFlags(Qt::FramelessWindowHint);
+	this->setAttribute(Qt::WA_TranslucentBackground, true);
+
+}
+
+void YuanShen_TrackFish::mousePressEvent(QMouseEvent *event)
+{
+	if (event->button() == Qt::LeftButton)
+	{
+		m_Press = event->globalPos();
+		leftBtnClk = true;
+	}
+	event->ignore();
+}
+
+void YuanShen_TrackFish::mouseReleaseEvent(QMouseEvent *event)
+{
+	if (event->button() == Qt::LeftButton)
+	{
+		leftBtnClk = false;
+	}
+	if (event->button() == Qt::RightButton)
+	{
+		this->close();
+	}
+	event->ignore();
+}
+
+void YuanShen_TrackFish::mouseMoveEvent(QMouseEvent *event)
+{
+	if (leftBtnClk)
+	{
+		m_Move = event->globalPos();
+		this->move(this->pos() + m_Move - m_Press);
+		m_Press = m_Move;
+	}
+	event->ignore();
+	update();
 }
--- YuanShen_TrackFish/YuanShen_TrackFish.h
+++ YuanShen_TrackFish/YuanShen_TrackFish.h
@@ -12,4 +12,24 @@ public:
 
 private:
     Ui::YuanShen_TrackFishClass ui;
+
+private:
+	QPoint m_Press;
+	QPoint m_Move;
+	bool leftBtnClk = false;
+
+	//HWND ysHandle;
+	//RECT ysRect;
+private:
+	void mousePressEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
+	void mouseReleaseEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
+	void mouseMoveEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
+	//void paintEvent(QPaintEvent *);
+
+	//void syncToYuanShen();
+	//void syncToThis();
+
+private slots:
+	//void StartMove();
+
 };
